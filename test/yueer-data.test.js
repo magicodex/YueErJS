68c7d4651ecdac9e0255e6749e7b29f02b75b473
zhuangmaohui <zhuangmaohui@163.com>
2022-12-24 00:38:12 +0800

    完善代码

--- test/yueer-data.test.js
+++ test/yueer-data.test.js
@@ -59,6 +59,60 @@ QUnit.test('yueer-data.data.util.isNullOrUndefined', function(assert) {
 //
 //
 //
+QUnit.test('yueer-data.data.getDataHandlerByElement', function(assert) {
+  var jqBaseElement = $('#qunit-fixture');
+  jqBaseElement.append('<p id="id1"></p>');
+  jqBaseElement.append('<span id="id2"></span>');
+  jqBaseElement.append('<table id="id3" data-type="grid"></span>');
+
+  var yerData = yer.data(jqBaseElement.get(0));
+  var element1 = $('#qunit-fixture').children('#id1')[0];
+  var element2 = $('#qunit-fixture').children('#id2')[0];
+  var element3 = $('#qunit-fixture').children('#id3')[0];
+
+  try {
+    yer.registerHandler('@span', { handlerName: 'handler1' });
+    yer.registerHandler('grid', { handlerName: 'handler2' })
+
+    // 默认处理器
+    {
+      var handler = yerData.getDataHandlerByElement(element1);
+      assert.strictEqual(handler.handlerName, 'default');
+    }
+
+    // 匹配到标签对应的处理器
+    {
+      var handler = yerData.getDataHandlerByElement(element2);
+      assert.strictEqual(handler.handlerName, 'handler1');
+    }
+
+    // 找到自定义的数据处理器
+    {
+      var handler = yerData.getDataHandlerByElement(element3);
+      assert.strictEqual(handler.handlerName, 'handler2');
+    }
+  } finally {
+    yer.Data.handlers = {};
+  }
+});
+
+//
+//
+//
+QUnit.test('yueer-data.data.queryElementBySelector', function(assert) {
+  var jqBaseElement = $('#qunit-fixture');
+  jqBaseElement.append('<input data-name="login.userName" data-type="input"/>');
+  jqBaseElement.append('<input data-name="login.password" data-type="password"/>');
+
+  var yerData = yer.data(jqBaseElement.get(0));
+  var elements = yerData.queryElementBySelector(['[data-name^="login."]'])
+
+  assert.strictEqual(elements.length, 2);
+});
+
+//
+//
+//
 QUnit.test('yueer-data.Data.convertExpressionToSelector', function(assert) {
   // 数组
   {
